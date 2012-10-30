ebff903966b50bd99106ec1b0bed6b131b6dd229
Chris Edwards <cmedwards@coe.edu>
2012-10-29 21:38:12 -0500

    Little done

--- readspecial.c
+++ readspecial.c
@@ -5,11 +5,17 @@
 
 struct special * read_struct(struct special *s, FILE *fp)
 {
-
+    fread(&s,sizeof(s),1,fp);
+    
 }
 
 int main(int argc, char *argv[])
 {
+    FILE *fp;
+    fp = open("special.data");
+    struct special p;
+    read_struct(s, fp);
 
+    fclose(fp);
     return 0;
 }
