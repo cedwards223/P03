6103946d165d5bcd8cdbf493141e82d72474ad65
Chris Edwards <cmedwards@coe.edu>
2012-10-30 16:56:21 -0500

    Final I think
    Just for now

--- readspecial.c
+++ readspecial.c
@@ -5,21 +5,26 @@
 
 struct special * read_struct(struct special *s, FILE *fp)
 {
-       if(fread(s,sizeof((*s).code),1,fp) == 0)
-           return 0;
-       else
-       {
-           (*s).word =  malloc(1* sizeof(char));
+    if(fread(s,sizeof(s->code),1,fp) == 0)
+        return 0;
+    else
+    {
+        s->word =  malloc(1* sizeof(char));
 
-           fread(s,sizeof(s.code),1,fp);
-           int i = 0;
-           while(fread(s,sizeof((*s).word),1,fp)!= "/n")
-           {
-               fread(s,sizeof((*s).word)[i],1,fp);
-           }
+        fread(s,sizeof(s->word),1,fp);
+        int i = 0;
+        int x = i;
+        while(s->word !=  "/n")
+        {
+            fread(s,sizeof(s->word)[i],1,fp);
+            s->word = malloc(sizeof(char)*x++);
+            i++;
+            x = i+1;
+        }
         return s;
-       }
+    }
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -27,18 +32,24 @@ int main(int argc, char *argv[])
     fp = fopen("special.data", "r");
 
     struct special *s;
-    
+
     while(!EOF)
     {
-        read_struct(&s, fp);
+        (*s).word = malloc(sizeof(char));
+        (*s).code = malloc(sizeof(int));
+        read_struct(&(*s), fp);
+
         if (read_struct == 0)
             break;
-        for (int i = 0; s.word[i] != "/n" ; i++)
+        int i = 0;
+        while ((*s).word[i] != "/n")
         {
-            printf(((*s).word)[i] - (*s).code);
+            printf("%c" , ((*s).word)[i] - ((*s).code));
+            i++;
         }
-
+        printf(" ");
     }
     fclose(fp);
+    printf("/n");
     return 0;
 }
