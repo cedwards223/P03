1be3b09d0a4d228aee90e6c9bc8345a2810e72a7
Chris Edwards <cmedwards@coe.edu>
2012-10-30 10:51:08 -0500

    Small inClass updates

--- readspecial.c
+++ readspecial.c
@@ -5,17 +5,24 @@
 
 struct special * read_struct(struct special *s, FILE *fp)
 {
-    fread(&s,sizeof(s),1,fp);
-    
+        fread(&s,sizeof(s),1,fp);
+        
 }
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
     fp = open("special.data");
-    struct special p;
-    read_struct(s, fp);
+    struct special s;
+    while(!EOF)
+    {
+        read_struct(&s, fp);
 
+        if (read_struct == 0)
+            break;
+
+        fwrite((*s),sizeof(
+    }
     fclose(fp);
     return 0;
 }
