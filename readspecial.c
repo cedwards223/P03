9c3e827283f926e11fd8e50591ac15de32432872
Chris Edwards <cmedwards@coe.edu>
2012-10-30 15:04:53 -0500

    Updates

--- readspecial.c
+++ readspecial.c
@@ -5,23 +5,39 @@
 
 struct special * read_struct(struct special *s, FILE *fp)
 {
-        fread(&s,sizeof(s),1,fp);
-        
+       if(fread(s,sizeof((*s).code),1,fp) == 0)
+           return 0;
+       else
+       {
+           (*s).word =  malloc(1* sizeof(char));
+
+           fread(s,sizeof(s.code),1,fp);
+           int i = 0;
+           while(fread(s,sizeof((*s).word),1,fp)!= "/n")
+           {
+               fread(s,sizeof((*s).word)[i],1,fp);
+           }
+        return s;
+       }
 }
 
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    fp = open("special.data");
-    struct special s;
+    fp = fopen("special.data", "r");
+
+    struct special *s;
+    
     while(!EOF)
     {
         read_struct(&s, fp);
-
         if (read_struct == 0)
             break;
+        for (int i = 0; s.word[i] != "/n" ; i++)
+        {
+            printf(((*s).word)[i] - (*s).code);
+        }
 
-        fwrite((*s),sizeof(
     }
     fclose(fp);
     return 0;
